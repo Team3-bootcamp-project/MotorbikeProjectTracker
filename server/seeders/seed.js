00788ec3273723f9daa40dd171ab924557b47561
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-05-17 18:30:06 -0700

    everything is terrible forever

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,26 +1,16 @@
 const db = require('../config/connection');
 const { Client, Project } = require('../models');
-const clientSeeds = require('./clientSeeds.json');
+// const clientSeeds = require('./clientSeeds.json');
 const projectSeeds = require('./projectSeeds.json');
 
 db.once('open', async () => {
   try {
     await Project.deleteMany({});
-    await Client.deleteMany({});
 
-    await Client.create(clientSeeds);
 
-    for (let i = 0; i < projectSeeds.length; i++) {
-      const { _id, projectName } = await Project.create(projectSeeds[i]);
-      const client = await Client.findOneAndUpdate(
-        // { email: email },
-        {
-          $addToSet: {
-            projects: _id,
-          },
-        }
-      );
-    }
+    await Project.create(projectSeeds);
+
+
   } catch (err) {
     console.error(err);
     process.exit(1);
