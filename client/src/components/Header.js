2633db3636bd869be9a160911d00a0e4b367358d
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-05-21 15:26:42 -0700

    header now changes when logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,13 @@ import React from "react";
 import { Box, Heading, HStack, Spacer, Button, ButtonGroup, Flex} from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { BrowserRouter as Router, Routes, Switch, Route, Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 function Header(){
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+      };
     return(
         <Box bg='none' shadow='md' w='100%' p={4} >
             <HStack>
@@ -12,22 +17,30 @@ function Header(){
 
                     <Spacer></Spacer>
                 <ButtonGroup gap='.03' variant='ghost' size='sm' mt={2}>
-
+                    
                         <Link to='/'>
                             <Button>
                                 Homepage
                             </Button>
                         </Link>
-
+                        {Auth.loggedIn() ? (
+                        <>
                         <Link to='/Login'>
                             <Button>
-                                Login
+                            {Auth.getProfile().data.username}
                             </Button>
                         </Link>
-
+                            <Button className="btn btn-lg btn-light m-2" onClick={logout}>
+                            Logout
+                            </Button>
+                            </>
+                           ) :(
+                               <Link to='/Login'>
                            <Button>
-                                 Gallery
+                                 Login
                            </Button>
+                           </Link>
+                           )}
                         <Link to='/Inquiries'>
                             <Button>
                                 Inquiries
