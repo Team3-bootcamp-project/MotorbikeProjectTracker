44dc0e80126c344ade3b7f94fb8911b6f4541e22
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-05-21 13:13:37 -0700

    importing stuff

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,41 @@ import {Homepage,
         Profile,
         SingleProject
         } from './pages';
+
+import {
+ApolloClient,
+InMemoryCache,
+ApolloProvider,
+createHttpLink,
+} from '@apollo/client';   
+import { setContext } from '@apollo/client/link/context';
+
 //import SingleProject from './SingleProject';
 // import Profile from './Profile';
 //import AdminProject from './pages/AdminProject';
 //import Inquiries from './Inquiries'
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
 function App() {
