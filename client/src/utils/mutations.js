a0dcda8327ec87a87bbb7c5bcae4e7ade743afe2
Miguel Torre <m.a.torre.f@gmail.com>
2022-05-20 18:06:54 -0700

    nothing of consequence

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -7,27 +7,25 @@ export const LOGIN_CLIENT = gql`
       token
       user {
         _id
-        username
-      }
+          }
     }
   }
 `;
 //This should only be called by the Admin user when creating a client for a project?
-export const ADD_USER = gql`
+export const ADD_CLIENT = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
-        username
-      }
+        }
     }
   }
 `;
 //Creating a project
-export const ADD_PROJECT = gql`
-  mutation addProject($projectName: String!) {
-    addThought(thoughtText: $thoughtText) {
+export const CREATE_PROJECT = gql`
+  mutation createProject($projectName: String!) {
+    createProject(projectName: $projectName) {
       _id
       projectName
       associatedClient
@@ -41,18 +39,9 @@ export const ADD_PROJECT = gql`
  //This one is for adding events to the timeline
  //called by admin user when recording the work that was done. Not sure how to do it
 
-export const ADD_EVENT = gql`
-  mutation addEvent($projectId: ID!, $description: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
+export const CREATE_EVENT = gql`
+  mutation createEvent($projectId: String!, $title: String!, $description: String!) {
+
     }
-  }
+  
 `;
