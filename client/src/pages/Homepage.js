b3ddb178bae4212eb60dfab0afea882ebe6ea6f6
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-05-22 13:32:03 -0700

    Modified homepage and created Project-List component to populate it

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -7,42 +7,21 @@ import {
   useQuery,
   gql
 } from "@apollo/client";
-// import {
-//   QUERY_PROJECTS
-// } from '../utils/queries.js'
+import ProjectList from "../components/ProjectList";
+import {
+  QUERY_PROJECTS
+} from '../utils/queries.js'
+
 // import { graphql } from 'react-apollo'
 
-function Feature({ title, desc, ...rest }) {
-    return (
-      <Box p={5} shadow='md' borderWidth='1px' {...rest}>
-        <Heading fontSize='xl'>{title}</Heading>
-        <Text mt={4}>{desc}</Text>
-      </Box>
 
-    )
-  }
+
+
   
   function Homepage() {
-    // console.log(this.props)
+
     return (
-      <VStack>
-      <Stack p={8} mt={8} spacing={8} direction='row'>
-        <Image boxSize='190px' shadow='md' src='picture.png' fallbackSrc='https://via.placeholder.com/150' />
-        <Feature
-          title='Bike Name'
-          desc='Bike Project Details'
-        />
-      </Stack>
-
-      <Stack p={8} spacing={8} direction='row'>
-        <Image boxSize='190px' shadow='md' src='picture.png' fallbackSrc='https://via.placeholder.com/150' />
-        <Feature
-          title='Bike Name'
-          desc='Bike Project Details'
-        />
-      </Stack>
-
-      </VStack>
+        <ProjectList />
       
     )
   }
